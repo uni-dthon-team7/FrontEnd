fa68964faeb7d9ddec8dd9ce3fb1362a69eb389f
ryeowon hong <ryeowon13@gmail.com>
2024-11-03 00:18:55 +0900

    update navbar

---
+++ src/assets/HomeIcon.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomeIcon = ({ className }) => (
+  <svg
+    width="64"
+    height="72"
+    viewBox="0 0 64 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      d="M32 0L0 24V72H20V44H44V72H64V24L32 0Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default HomeIcon;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,40 @@
 import React from "react";
-import homeIcon from "../assets/home.svg";
-import searchIcon from "../assets/search.svg";
-import bookmarkIcon from "../assets/bookmark.svg";
+import { Link, useLocation } from "react-router-dom";
+import HomeIcon from "../assets/HomeIcon";
+import SearchIcon from "../assets/SearchIcon";
+import BookmarkIcon from "../assets/BookmarkIcon";
 
 const NavBar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
-    <nav className="navbar absolute bottom-0 h-14 w-full bg-[#5D5D5D] flex justify-center left-0 right-0">
+    <nav className="navbar absolute bottom-0 h-14 w-full flex justify-center left-0 right-0 border-t">
       <div className="flex items-center justify-around w-full">
-        <img src={searchIcon} alt="search" className="w-5 h-5" />
-        <img src={homeIcon} alt="home" className="w-5 h-5" />
-        <img src={bookmarkIcon} alt="bookmark" className="w-5 h-5" />
+        <Link to="/search">
+          <SearchIcon
+            alt="search"
+            className={`w-5 h-5 ${
+              currentPath === "/search" ? "text-black" : "text-gray-500"
+            }`}
+          />
+        </Link>
+        <Link to="/">
+          <HomeIcon
+            alt="home"
+            className={`w-5 h-5 ${
+              currentPath === "/" ? "text-black" : "text-gray-500"
+            }`}
+          />
+        </Link>
+        <Link to="/bookmark">
+          <BookmarkIcon
+            alt="bookmark"
+            className={`w-5 h-5 ${
+              currentPath === "/bookmark" ? "text-black" : "text-gray-500"
+            }`}
+          />
+        </Link>
       </div>
     </nav>
   );
