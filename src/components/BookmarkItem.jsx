ba758d808a855d4cfe64125cfeba8562e2191ef0
ryeowon hong <ryeowon13@gmail.com>
2024-11-02 23:07:49 +0900

    Style bookmark item

--- src/components/BookmarkItem.jsx
+++ src/components/BookmarkItem.jsx
@@ -1,15 +1,17 @@
 const BookmarkItem = ({ bookmark }) => {
   return (
-    <div className="flex items-center bg-white rounded-md h-18 p-2 shadow-custom m-2 w-max">
-      <div className="font-semibold text-base whitespace-nowrap ml-3 flex-grow">
+    <div className="flex items-center bg-white rounded-md h-18 p-2 shadow-custom m-2 w-full flex-nowrap">
+      <div className="font-semibold text-base whitespace-nowrap ml-3">
         {bookmark.name}
       </div>
-      <span className="text-sm mx-2 whitespace-nowrap">3일전</span>
-      <img
-        src={bookmark.image}
-        alt={bookmark.name}
-        className="w-14 h-14 rounded-lg m-1"
-      />
+      <span className="text-sm mx-3 whitespace-nowrap text-[#ACACAC]">3일전</span>
+      <div className="w-14 mr-2">
+        <img
+          src={bookmark.image}
+          alt={bookmark.name}
+          className="w-14 h-14 rounded-lg m-1 block"
+        />
+      </div>
     </div>
   );
 };
